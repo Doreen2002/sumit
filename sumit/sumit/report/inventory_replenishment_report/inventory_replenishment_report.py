49a7aaa12e99d138d16641fe03f3f5fb1c57cd7d
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-04-14 14:06:40 +0200

    Update inventory_replenishment_report.py

--- sumit/sumit/report/inventory_replenishment_report/inventory_replenishment_report.py
+++ sumit/sumit/report/inventory_replenishment_report/inventory_replenishment_report.py
@@ -4,6 +4,8 @@
 import frappe
 from frappe import _, msgprint
 from erpnext.stock.report.stock_balance.stock_balance import execute as stock_balance
+from datetime import datetime
+from frappe.utils import date_diff
 
 
 
@@ -13,18 +15,26 @@ def execute(filters=None):
 	data = []
 	coloumns = [
 		{
+			"fieldname":"warehouse",
+			"label": "Warehouse",
+			"fieldtype": "Link",
+			"options":"Warehouse",
+			"width": 200
+		},
+		{
+			"fieldname": "item_name",
+			"label": "Item Name",
+			"fieldtype": "Data",
+			"width": 200
+		},
+		{
 			"fieldname": "item_code",
 			"label": "Item Code",
 			"fieldtype": "Link",
 			"options": "Item",
 			"width": 200
 		},
-		{
-			"fieldname": "item_group",
-			"label": "Item Group",
-			"fieldtype": "Data",
-			"width": 200
-		},
+		
 		{
 			"fieldname": "uom",
 			"label": "UOM",
@@ -34,7 +44,7 @@ def execute(filters=None):
 		{
 			"fieldname": "reserved",
 			"label": "Reserved",
-			"fieldtype": "Data",
+			"fieldtype": "Float",
 			"width": 100
 		},
 
@@ -47,7 +57,7 @@ def execute(filters=None):
 		{
 			"fieldname": "avg_consumption",
 			"label": "Avg Consumption",
-			"fieldtype": "Data",
+			"fieldtype": "Float",
 			"width": 100
 		},
 		{
@@ -57,9 +67,15 @@ def execute(filters=None):
 			"width": 100
 		},
 		{
+			"fieldname": "custom_safety_time_in_days",
+			"label": "Safety Time in Days",
+			"fieldtype": "Float",
+			"width": 100
+		},
+		{
 			"fieldname": "qty_to_order",
 			"label": "Qty To Order",
-			"fieldtype": "Data",
+			"fieldtype": "Float",
 			"width": 100
 		}
 	]
@@ -70,94 +86,61 @@ def execute(filters=None):
 	
 	data = get_data(filters)
 	return coloumns, data
-
-	
-def data_with_qty_to_order(filters):
-	data =[]
-	wh = filters.get("warehouse")
-
-	if wh:
-		stock = frappe.db.get_all("Bin", filters={"warehouse": wh}, fields=["item_code", "actual_qty",  "stock_uom", "reserved_qty"])
-		for st in stock:
-			item_code = st.item_code
-			item_name = frappe.db.get_value("Item", item_code, "item_name")
-			item_group = frappe.db.get_value("Item", item_code, "item_group")
-			uom = st.stock_uom
-			stock_balance = st.actual_qty
-			lead_time = frappe.db.get_value("Item", item_code, "lead_time_days")
-			safety_stock = frappe.db.get_value("Item", item_code, "safety_stock")
-
-			#average consumption or out quantity for a time period
-			avg_consumption = get_average_consumption(filters, item_code)
-			reserve_stock = int(lead_time)
-			if int(safety_stock) > int(lead_time):
-				reserve_stock =  int(safety_stock)
-			qty_to_order = int((reserve_stock - stock_balance) * avg_consumption )
-			if qty_to_order < 0:
-				qty_to_order = 0
-			
-	
-	
-			if qty_to_order > 0:
-				data.append({
-					"item_group": item_group,
-					"item_code": item_code,
-					"uom": uom,
-					"reserved": reserve_stock,
-					"stock": stock,
-					"balance_stock": stock_balance,
-					"avg_consumption": round(avg_consumption, 2),
-					"lead_time": lead_time,
-					"qty_to_order": filters.get("show_qty_to_order")
-				})
-			
-		# if filters.get("show_qty_to_order"):
-		# 	return data
-		# else:
-		return data
+def date_converter_month(date_str):
+    date_obj = datetime.strptime(date_str, "%d/%m/%Y")
+    formatted_date = date_obj.strftime("%Y-%m-%d")
+    return formatted_date
 	
 def get_data(filters):
+
 	data =[]
 	wh = filters.get("warehouse")
-
+	item_filters={}
+	stock_ledger_filters = {}
+	if filters.get("item_group"):
+		item_filters["item_group"] = filters.get("item_group")
+	if filters.get("item_code"):
+		item_filters["item_code"] = filters.get("item_code")	
+	if filters.get("from_date") and filters.get("to_date"):
+		stock_ledger_filters["posting_date"] = ['between', [filters.get("from_date"), filters.get("to_date")]]
+		#stock_ledger_filters["posting_date"] = ['between', [date_converter_month(filters.get("from_date")), date_converter_month(filters.get("to_date"))]]
+	# frappe.throw(f"{filters.get("from_date")}")
+	# pass
 	if wh:
-		stock = frappe.db.get_all("Bin", filters={"warehouse": wh}, fields=["item_code", "actual_qty",  "stock_uom", "reserved_qty"])
-		for st in stock:
-			item_code = st.item_code
-			item_name = frappe.db.get_value("Item", item_code, "item_name")
-			item_group = frappe.db.get_value("Item", item_code, "item_group")
-			uom = st.stock_uom
-			stock_balance = st.actual_qty
-			lead_time = frappe.db.get_value("Item", item_code, "lead_time_days")
-			safety_stock = frappe.db.get_value("Item", item_code, "safety_stock")
-
-			#average consumption or out quantity for a time period
-			avg_consumption = get_average_consumption(filters, item_code)
-			reserve_stock = int(lead_time)
-			if int(safety_stock) > int(lead_time):
-				reserve_stock =  int(safety_stock)
-			qty_to_order = int((reserve_stock - stock_balance) * avg_consumption )
-			if qty_to_order < 0:
-				qty_to_order = 0
-
+		items = frappe.db.get_list("Item", filters=item_filters, fields=["*"])
+		for item in items: 
+			stock = 0
+			average_consup = 0
+			reserved_stock = 0
+			qty_to_order = 0
+			difference = date_diff(filters.get("to_date") , filters.get("from_date"))
+			for s in frappe.db.get_all("Bin", filters={"warehouse": wh, "item_code": item.item_code}, fields=["item_code", "actual_qty",  "stock_uom", "reserved_qty"]):
+				stock+=s.actual_qty
+			stock_ledger_filters["item_code"] = item.item_code
+			stock_ledger_filters["warehouse"] = filters.get("warehouse")
+			for r in frappe.db.get_list("Stock Ledger Entry", filters=stock_ledger_filters, fields=["*"]):
+				if r.actual_qty < 0:
+					average_consup -= r.actual_qty
+			avg = average_consup/difference	
+			if item.custom_safety_time_in_days > item.lead_time_days:
+				reserved_stock = avg * item.custom_safety_time_in_days
+			if item.lead_time_days > item.custom_safety_time_in_days:
+				reserved_stock = avg * item.lead_time_days
+			if stock < reserved_stock: 
+				qty_to_order = reserved_stock - stock
 			data.append({
-					"item_group": item_group,
-					"item_code": item_code,
-					"uom": uom,
-					"reserved": reserve_stock,
-					"stock": stock,
-					"balance_stock": stock_balance,
-					"avg_consumption": round(avg_consumption, 2),
-					"lead_time": lead_time,
+					"warehouse": filters.get("warehouse"),
+					"item_name": item.item_name,
+					"item_code": item.item_code,
+					"uom": item.stock_uom,
+					"reserved": reserved_stock,
+					"balance_stock": stock,
+					"avg_consumption": avg,
+					"lead_time": item.lead_time_days,
+					"custom_safety_time_in_days": item.custom_safety_time_in_days,
 					"qty_to_order": qty_to_order
 				})
-	
-	
-			
-			
-		
-		return data
-	
+	return data
 
 def get_average_consumption(filters, item_code):
         #get stock ledger entries where quanity change is negative
